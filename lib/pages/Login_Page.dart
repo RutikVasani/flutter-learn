9123f4b9d87a93d96b22eac0220da53da3e0875a
Rutik <rutikvasani370@gmail.com>
2021-08-18 16:23:07 +0530

    day 5 completed

--- lib/pages/Login_Page.dart
+++ lib/pages/Login_Page.dart
@@ -1,7 +1,15 @@
 import 'package:chat_application_1/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +25,7 @@ class LoginPage extends StatelessWidget {
                 height: 30,
               ),
               Text(
-                "welcome",
+                "welcome $name",
                 style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
@@ -34,6 +42,10 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter user name", labelText: "User name"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -45,19 +57,56 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 50,
                     ),
-                    ElevatedButton(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                      onPressed: () {
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeroutes);
-                        print(
-                          "hello student",
-                        );
                       },
-                    )
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 60 : 150,
+                        height: changeButton ? 60 : 50,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                                size: 30,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: changeButton
+                                      ? Colors.pinkAccent
+                                      : Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                ),
+                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 60 : 8),
+                        ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   child: Text(
+                    //     "Login",
+                    //     style: TextStyle(fontSize: 20),
+                    //   ),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeroutes);
+                    //     print(
+                    //       "hello student",
+                    //     );
+                    //   },
+                    // )
                   ],
                 ),
               )
